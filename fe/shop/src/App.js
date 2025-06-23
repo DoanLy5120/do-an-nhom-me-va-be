9df3862e7be8584ef313cbf1db2e2639df597a4a
DoanLy5120 <doanthaoly.hkqn@gmail.com>
2025-06-24 00:44:15 +0700

    cấu trúc và tải thư viện

--- fe/shop/src/App.js
+++ fe/shop/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Tabs } from 'antd';
 
+const onChange = key => {
+  console.log(key);
+};
+const items = [
+  {
+    key: '1',
+    label: 'Tab 1',
+    children: 'Content of Tab Pane 1',
+  },
+  {
+    key: '2',
+    label: 'Tab 2',
+    children: 'Content of Tab Pane 2',
+  },
+  {
+    key: '3',
+    label: 'Tab 3',
+    children: 'Content of Tab Pane 3',
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
   );
 }
 
